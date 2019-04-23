7a0c7005b410f01fccbafca4f0f217be11fdf956
yuezhaocanghai <47106599+yuezhaocanghai@users.noreply.github.com>
2019-04-23 20:06:40 +0800

    Create unit4_ex4.3.py

---
+++ unit4_ex4.3.py
@@ -0,0 +1,12 @@
+#练习4.3-1
+import turtle
+bob = turtle.Turtle()
+
+def square(t):
+    for i in range(4):
+        bob.fd(100)
+        bob.lt(90)
+    turle.mainloop()
+    
+print(square(bob))
+      
